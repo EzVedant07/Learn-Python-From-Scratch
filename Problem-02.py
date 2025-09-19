4a00e49773aea86cebcf281c492ef63975d4478d
EzVedant07 <patelvedant0914@gmail.com>
2025-09-19 15:58:26 +0530

    Add files via upload

---
+++ Problem-02.py
@@ -0,0 +1,16 @@
+# # Using REPL (Read–Evaluate–Print–Loop) to print table of 5
+# # In REPL, we type one expression at a time and Python executes it immediately.
+
+# >>> for i in range(1, 11):     # Loop from 1 to 10
+# ...     print(5, "x", i, "=", 5*i)   # Print multiplication in table format
+# ...
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 4 = 20
+# 5 x 5 = 25
+# 5 x 6 = 30
+# 5 x 7 = 35
+# 5 x 8 = 40
+# 5 x 9 = 45
+# 5 x 10 = 50
